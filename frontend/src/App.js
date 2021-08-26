e18bcb4f5c1e8f284cfd191d00ebc829a6b0c1d5
iamjacek <jake47s@gmail.com>
2021-08-26 16:14:44 +0100

    payment screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import SigninScreen from "./Screens/SigninScreen"
 import { signout } from "./actions/userActions"
 import RegisterScreen from "./Screens/RegisterScreen"
 import ShippingAddressScreen from "./Screens/ShippingAddressScreen"
+import PaymentMethodScreen from "./Screens/PaymentMethodScreen"
 function App() {
   const cart = useSelector((state) => state.cart)
   const { cartItems } = cart
@@ -55,6 +56,7 @@ function App() {
           <Route path="/signin" component={SigninScreen}></Route>
           <Route path="/register" component={RegisterScreen}></Route>
           <Route path="/shipping" component={ShippingAddressScreen} />
+          <Route path="/payment" component={PaymentMethodScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
         </main>
         <footer className="row center">All right reserved</footer>
