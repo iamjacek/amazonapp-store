677bc17056622e60947138a3365c6104916856fb
iamjacek <jake47s@gmail.com>
2021-09-24 19:29:29 +0100

    add admin menu

--- backend/utils.js
+++ backend/utils.js
@@ -30,3 +30,11 @@ export const isAuth = (req, res, next) => {
     req.status(401).send({ message: "No token" })
   }
 }
+
+export const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next()
+  } else {
+    res.status(401).send({ message: "Invalid Admin Token" })
+  }
+}
