387a50ac081394a9f08a853fc1679cc0cbd9b03f
iamjacek <iamjacek@icloud.com>
2021-11-08 20:25:24 +0000

    fill datajs and update product router with seller data,fix logic in userRouter

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -146,10 +146,8 @@ userRouter.put(
     if (user) {
       user.name = req.body.name || user.name;
       user.email = req.body.email || user.email;
-      user.isSeller =
-        req.body.isSeller === user.isSeller ? user.isSeller : req.body.isSeller;
-      user.isAdmin =
-        req.body.isAdmin === user.isAdmin ? user.isAdmin : req.body.isAdmin;
+      user.isSeller = Boolean(req.body.isSeller);
+      user.isAdmin = Boolean(req.body.isAdmin);
       const updateUser = await user.save();
       res.send({ message: "User Updated", user: updateUser });
     } else {
