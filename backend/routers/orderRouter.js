f15fd70f3c0e535ce2b92cd9bfed3a73f65b5176
iamjacek <iamjacek@icloud.com>
2021-11-12 18:31:16 +0000

    config aws s3 for upload

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -1,7 +1,13 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import Order from "../models/orderModel.js";
-import { isAdmin, isAuth, isSellerOrAdmin } from "../utils.js";
+import {
+  isAdmin,
+  isAuth,
+  isSellerOrAdmin,
+  mailgun,
+  payOrderEmailTemplate,
+} from "../utils.js";
 
 const orderRouter = express.Router();
 
@@ -79,7 +85,7 @@ orderRouter.put(
   "/:id/pay",
   isAuth,
   expressAsyncHandler(async (req, res) => {
-    const order = await Order.findById(req.params.id);
+    const order = await Order.findById(req.params.id).populate("user", "email");
     if (order) {
       order.isPaid = true;
       order.paidAt = Date.now();
@@ -90,6 +96,25 @@ orderRouter.put(
         email_address: req.body.email_address,
       };
       const updatedOrder = await order.save();
+      //send email to user
+      mailgun()
+        .messages()
+        .send(
+          {
+            from: "Amazon-app <info@mg.yourdomain.com>",
+            to: `${order.user.name} <${order.user.email}>`,
+            subject: `New order ${order._id}`,
+            html: payOrderEmailTemplate(order),
+          },
+          (error, body) => {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log(body);
+            }
+          }
+        );
+
       res.send({ message: "Order Paid", order: updatedOrder });
     } else {
       res.status(404).send({ message: "Order Not Found" });
--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,51 +1,51 @@
-import React, { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import Axios from "axios"
-import { detailsProduct, updateProduct } from "../actions/productActions"
-import LoadingBox from "../components/LoadingBox"
-import MessageBox from "../components/MessageBox"
-import { PRODUCT_UPDATE_RESET } from "../constants/productConstants"
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Axios from "axios";
+import { detailsProduct, updateProduct } from "../actions/productActions";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { PRODUCT_UPDATE_RESET } from "../constants/productConstants";
 
 export default function ProductEditScreen(props) {
-  const productId = props.match.params.id
-  const [name, setName] = useState("")
-  const [price, setPrice] = useState("")
-  const [image, setImage] = useState("")
-  const [category, setCategory] = useState("")
-  const [countInStock, setCountInStock] = useState("")
-  const [brand, setBrand] = useState("")
-  const [description, setDescription] = useState("")
+  const productId = props.match.params.id;
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+  const [category, setCategory] = useState("");
+  const [countInStock, setCountInStock] = useState("");
+  const [brand, setBrand] = useState("");
+  const [description, setDescription] = useState("");
 
-  const productDetails = useSelector((state) => state.productDetails)
-  const { loading, error, product } = productDetails
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
 
-  const productUpdate = useSelector((state) => state.productUpdate)
+  const productUpdate = useSelector((state) => state.productUpdate);
   const {
     loading: loadingUpdate,
     error: errorUpdate,
     success: successUpdate,
-  } = productUpdate
+  } = productUpdate;
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   useEffect(() => {
     if (successUpdate) {
-      props.history.push("/productlist")
+      props.history.push("/productlist");
     }
     if (!product || product._id !== productId || successUpdate) {
-      dispatch(detailsProduct(productId))
-      dispatch({ type: PRODUCT_UPDATE_RESET })
+      dispatch(detailsProduct(productId));
+      dispatch({ type: PRODUCT_UPDATE_RESET });
     } else {
-      setName(product.name)
-      setPrice(product.price)
-      setImage(product.image)
-      setCategory(product.category)
-      setCountInStock(product.countInStock)
-      setBrand(product.brand)
-      setDescription(product.description)
+      setName(product.name);
+      setPrice(product.price);
+      setImage(product.image);
+      setCategory(product.category);
+      setCountInStock(product.countInStock);
+      setBrand(product.brand);
+      setDescription(product.description);
     }
-  }, [product, dispatch, productId, successUpdate, props.history])
+  }, [product, dispatch, productId, successUpdate, props.history]);
   const submitHandler = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     dispatch(
       updateProduct({
         _id: productId,
@@ -57,30 +57,31 @@ export default function ProductEditScreen(props) {
         countInStock,
         description,
       })
-    )
-  }
-  const [loadingUpload, setLoadingUpload] = useState(false)
-  const [errorUpload, setErrorUpload] = useState(false)
-  const userSignin = useSelector(state=> state.userSignin)
-  const {userInfo} = userSignin
-  const uploadFileHandler = async(e) => {
-    const file = e.target.files[0]
-    const bodyFormData = new FormData()
-    bodyFormData.append('image',  file)
-    setLoadingUpload(true)
-    try { 
-      const { data } = await Axios.post('/api/uploads', bodyFormData, {
-        headers: {'Content-Type':"multipart/form-data",
-      Authorization: `Bearer ${userInfo.token}`
-      }
-      })
-      setImage(data)
-      setLoadingUpload(false)
+    );
+  };
+  const [loadingUpload, setLoadingUpload] = useState(false);
+  const [errorUpload, setErrorUpload] = useState(false);
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const uploadFileHandler = async (e) => {
+    const file = e.target.files[0];
+    const bodyFormData = new FormData();
+    bodyFormData.append("image", file);
+    setLoadingUpload(true);
+    try {
+      const { data } = await Axios.post("/api/uploads/s3", bodyFormData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      });
+      setImage(data);
+      setLoadingUpload(false);
     } catch (error) {
-      setErrorUpload(error.message)
-      setLoadingUpload(false)
+      setErrorUpload(error.message);
+      setLoadingUpload(false);
     }
-  }
+  };
   return (
     <div>
       <form className="form" onSubmit={submitHandler}>
@@ -124,13 +125,17 @@ export default function ProductEditScreen(props) {
                 value={image}
                 onChange={(e) => setImage(e.target.value)}
               ></input>
-              </div>
-              <div>
-                <label htmlFor="imageFile">Image File</label>
-                <input type="file" id="imageFile" label="Choose Image" onChange={uploadFileHandler} />
-                {loadingUpload && <LoadingBox />}
+            </div>
+            <div>
+              <label htmlFor="imageFile">Image File</label>
+              <input
+                type="file"
+                id="imageFile"
+                label="Choose Image"
+                onChange={uploadFileHandler}
+              />
+              {loadingUpload && <LoadingBox />}
               {errorUpload && <MessageBox variant="danger">{error}</MessageBox>}
-              
             </div>
             <div>
               <label htmlFor="category">Category</label>
@@ -183,5 +188,5 @@ export default function ProductEditScreen(props) {
         )}
       </form>
     </div>
-  )
+  );
 }
